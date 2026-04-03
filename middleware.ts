d2d922e507dde4053d22ec0627f93ea64d6b263a
macaril <lutfiardiansyah371@gmail.com>
2026-04-03 15:33:51 +0700

    update middle

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export function middleware(request: NextRequest) {
-  const auth = request.cookies.get('admin-session'); // Contoh sederhana pakai cookie
+  const auth = request.cookies.get('admin-auth'); // Sesuai dengan nama cookie di adminAuth.ts
 
   if (request.nextUrl.pathname.startsWith('/admin')) {
     if (!auth) {
